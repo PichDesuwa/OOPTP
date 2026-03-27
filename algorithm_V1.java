8d729d2024e89f7aa7cb7c79170b6b7d1e56a6b5
PichDesuwa <pichbothun@gmail.com>
2026-03-27 14:49:37 +0700

    task

---
+++ algorithm_V1.java
@@ -0,0 +1,30 @@
+public class algorithm_V1 {
+    //task1
+    int[] sample ={};
+    
+    int[] input;
+    int[] output;
+
+    //task2
+    public algorithm_V1(int[] input) {
+
+    }
+    
+    //task3
+    int[] sum(int[] input){
+        return new int[0];
+    }
+    
+    //task4
+    int max(int[] input){
+        return 0;
+    }
+
+    //task5
+    int min(int[] input){
+        return 0;
+    }
+
+    //adadaa
+
+}
