e14409e613ceeb7329596a9aae9fdde081320d98
mAiyc <lyzatouy@gmail.com>
2026-04-03 13:55:05 +0700

    new code

--- MAIN.java
+++ MAIN.java
@@ -1,3 +1,8 @@
 public class MAIN {
+    public static void main(String[] args) {
+        int [] input = {1, 2, 3, 4, 5, 6};
+        algorithm_V1 algo = new algorithm_V1(input);
+        System.out.println("Sum: " + algo.sum(input));
+    }
     
 }
