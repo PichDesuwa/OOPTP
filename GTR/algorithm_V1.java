3b897d01c86e28f3bd286771da0e7fbfb29c3893
PichDesuwa <pichbothun@gmail.com>
2026-04-03 14:10:38 +0700

    Task for everyone

--- GTR/algorithm_V1.java
+++ GTR/algorithm_V1.java
@@ -1,33 +1,10 @@
 package GTR;
 public class algorithm_V1 {
-    //task1
-    int[] sample ={1,2,3,4,5,6};
-    
-    int[] input;
-    int[] output;
+    //Dara ther attribute
 
-    //task2
-    public algorithm_V1(int[] input) {
-        this.input = input;
-    }
-    
-    //task3
-    int[] sum(int[] input){
-        return new int[0];
-    }
-    
-    //task4
-    int max(int[] input){
-        return 0;
-    }
+    //Phanith ther constructor
 
-    //task5
-    int min(int[] input){
-        return 0;
-    }
+    //Lyza ther sum
 
-    //papa im hungry pls feed me pizza
-    // i'm gtr
-    // My salary 150$ wanna buy GTR_R32
-// eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee/
+    //Both ther print
 }
