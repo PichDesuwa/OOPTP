8eb83619bf53b236fc5ea5bf7b95bf4c76af00af
saydara2004-source <say.dara2004@gmail.com>
2026-04-03 14:49:40 +0700

    Update MAIN.java

--- MAIN.java
+++ MAIN.java
@@ -3,7 +3,7 @@ import GTR.algorithm_V1;
 public class MAIN {
     public static void main(String[] args) {
         int [] input = {1, 2, 3, 4, 5, 6};
-        algorithm_V1 algo = new algorithm_V1(input);
+        algorithm_V1 algo = new algorithm_V1();
         System.out.println("Sum: " + algo.sum(input));
     }
     
