34419b5e7d1b3833349dd7195a34721745443306
PichDesuwa <pichbothun@gmail.com>
2026-04-03 14:36:50 +0700

    Update1

--- GTR/algorithm_V1.java
+++ GTR/algorithm_V1.java
@@ -1,10 +1,13 @@
 package GTR;
 public class algorithm_V1 {
     //Dara ther attribute
-
+    int[] sample = {2,4,6,8};
     //Phanith ther constructor
-
+    public algorithm_V1(int[] sample){
+        this.sample = sample;
+    }
     //Lyza ther sum
+
 
     //Both ther print
 }
